45429de40c28d6f3d4f5d07d00c22de8526aaae2
Lenin Figueroa <lenin.figa@gmail.com>
2018-05-25 22:32:55 -0500

    wea

--- TVFPS/Assets/Script/Vida.cs
+++ TVFPS/Assets/Script/Vida.cs
@@ -4,17 +4,61 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Vida : MonoBehaviour {
-	private int saludJ=1000;
+    private int saludJ = 1000;
+    bool isVulnerable;
+    public GameObject GameOverMenuUI;
+    public GameObject Jugador;
+    public GameObject JuegoUI;
 
-	public Text UIsaludJ;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    public Text UIsaludJ;
+    // Use this for initialization
+    void Start()
+    {
+        isVulnerable = true;
+        GameOverMenuUI.SetActive(false);
+    }
 
-		UIsaludJ.text = saludJ.ToString ();
-	}
+    // Update is called once per frame
+    void Update()
+    {
+
+        UIsaludJ.text = saludJ.ToString();
+        if (saludJ <= 0)
+        {
+            Time.timeScale = 0f;
+            Jugador.SetActive(false);
+            JuegoUI.SetActive(false);
+            GameOverMenuUI.SetActive(true);
+        }
+    }
+
+    void OnCollisionEnter(Collision _other)
+    {
+        Debug.Log("Me esta tocando " + _other.transform.tag);
+        if (_other.transform.tag == "enemigo" && isVulnerable == true)
+        {
+            StartCoroutine(Daño());
+            Debug.Log("me hicieron daño");
+        }
+
+    }
+
+    void OnTriggerEnter(Collider _other)
+    {
+        Debug.Log("Me esta tocando" + _other.transform.tag);
+        if (_other.transform.tag == "enemigo" && isVulnerable == true)
+        {
+            StartCoroutine(Daño());
+            Debug.Log("me hicieron daño");
+        }
+    }
+
+    IEnumerator Daño()
+    {
+        saludJ -= 50;
+        isVulnerable = false;
+
+        yield return new WaitForSeconds(.5f);
+        isVulnerable = true;
+    }
 }
---
+++ TVFPS/Assets/UI/Script/UIjuego.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIjuego : MonoBehaviour {
+    public static bool GameIsPaused = false;
+    public GameObject PauseMenuUI;
+    public GameObject Jugador;
+    public GameObject JuegoUI;
+
+
+
+
+
+    void Start()
+    {
+
+        PauseMenuUI.SetActive(false);
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+
+    }
+
+    public void Resume()
+    {
+        PauseMenuUI.SetActive(false);
+        Jugador.SetActive(true);
+        JuegoUI.SetActive(true);
+
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+
+    }
+    public void Pause()
+    {
+        Jugador.SetActive(false);
+        JuegoUI.SetActive(false);
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void Reiniciar()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(1);
+    }
+    public void CargarMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(0);
+    }
+    public void QuitarJuego()
+    {
+        Application.Quit();
+
+    }
+}
+
